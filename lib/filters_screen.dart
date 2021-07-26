2bec526c30e2fe554d00e4bdf8932106f4466c11
Sadid369 <sadid.jones@gmail.com>
2021-07-26 20:29:39 +0600

    day 5 completed

--- lib/filters_screen.dart
+++ lib/filters_screen.dart
@@ -1,18 +1,117 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/main_drawer.dart';
 
-class FilterScreen extends StatelessWidget {
+class FilterScreen extends StatefulWidget {
   static const routeName = '/filters';
+  final Function saveFilters;
+  final Map<String, bool> currentFilters;
+
+  FilterScreen({this.saveFilters, this.currentFilters});
+
+  @override
+  _FilterScreenState createState() => _FilterScreenState();
+}
+
+class _FilterScreenState extends State<FilterScreen> {
+  bool _glutenFree = false;
+  bool _vegetarian = false;
+  bool _vegan = false;
+  bool _lactoseFree = false;
+
+  @override
+  void initState() {
+    _glutenFree = widget.currentFilters['gluten'];
+    _vegan = widget.currentFilters['vegan'];
+    _lactoseFree = widget.currentFilters['lactose'];
+    _vegetarian = widget.currentFilters['vegetarian'];
+    super.initState();
+  }
+
+  Widget _buildSwitchListTile(String title, String description,
+      bool currentValue, Function updateValue) {
+    return SwitchListTile(
+      title: Text(title),
+      subtitle: Text(description),
+      value: currentValue,
+      onChanged: updateValue,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: MainDrawer(),
-      appBar: AppBar(
-        title: Text('Your Fiters'),
-      ),
-      body: Center(
-        child: Text('Filters'),
-      ),
-    );
+        drawer: MainDrawer(),
+        appBar: AppBar(
+          actions: [
+            IconButton(
+                onPressed: () {
+                  Map<String, bool> selectedfilters = {
+                    'gluten': _glutenFree,
+                    'lactose': _lactoseFree,
+                    'vegan': _vegan,
+                    'vegetarian': _vegetarian,
+                  };
+                  widget.saveFilters(selectedfilters);
+                },
+                icon: Icon(Icons.save))
+          ],
+          title: Text('Your Fiters'),
+        ),
+        body: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(20),
+              child: Text(
+                'Adjust your meal selection',
+                style: Theme.of(context).textTheme.title,
+              ),
+            ),
+            Expanded(
+                child: ListView(
+              children: [
+                _buildSwitchListTile(
+                  'Gluten-Free',
+                  'Only Included Gluten Free Meal',
+                  _glutenFree,
+                  (newValue) {
+                    setState(() {
+                      _glutenFree = newValue;
+                    });
+                  },
+                ),
+                _buildSwitchListTile(
+                  'Vegetarian',
+                  'Only Included Vegetarian  Meal',
+                  _vegetarian,
+                  (newValue) {
+                    setState(() {
+                      _vegetarian = newValue;
+                    });
+                  },
+                ),
+                _buildSwitchListTile(
+                  'Vegan',
+                  'Only Included Vegan  Meal',
+                  _vegan,
+                  (newValue) {
+                    setState(() {
+                      _vegan = newValue;
+                    });
+                  },
+                ),
+                _buildSwitchListTile(
+                  'lactose-Free',
+                  'Only Included lactose-Free  Meal',
+                  _lactoseFree,
+                  (newValue) {
+                    setState(() {
+                      _lactoseFree = newValue;
+                    });
+                  },
+                ),
+              ],
+            ))
+          ],
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,49 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:meals_app/category_meals_screen.dart';
+import 'package:meals_app/dummy_data.dart';
 import 'package:meals_app/filters_screen.dart';
 import 'package:meals_app/meal_detail_screen.dart';
 import 'package:meals_app/categories_screen.dart';
+import 'package:meals_app/models/meal.dart';
 import 'package:meals_app/tabs_screen.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Map<String, bool> _filters = {
+    'gluten': false,
+    'lactose': false,
+    'vegan': false,
+    'vegetarian': false,
+  };
+  List<Meal> _availableMeals = DUMMY_MEALS;
+  void _setFilters(Map<String, bool> filterData) {
+    setState(() {
+      _filters = filterData;
+      _availableMeals = DUMMY_MEALS.where((meal) {
+        if (_filters['gluten'] == true && meal.isGlutenFree == false) {
+          return false;
+        }
+        if (_filters['lactose'] == true && meal.isLactoseFree == false) {
+          return false;
+        }
+        if (_filters['vegan'] == true && meal.isVegan == false) {
+          return false;
+        }
+        if (_filters['vegetarian'] == true && meal.isVegetarian == false) {
+          return false;
+        }
+        return true;
+      }).toList();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -35,9 +70,14 @@ class MyApp extends StatelessWidget {
       //home: TabsScreen(),
       routes: {
         '/': (ctx) => TabsScreen(),
-        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(
+              availableMeals: _availableMeals,
+            ),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
-        FilterScreen.routeName: (ctx) => FilterScreen(),
+        FilterScreen.routeName: (ctx) => FilterScreen(
+              saveFilters: _setFilters,
+              currentFilters: _filters,
+            ),
       },
       onUnknownRoute: (setting) {
         return MaterialPageRoute(
